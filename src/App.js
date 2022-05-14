462b149f65ccd83c5dd7f99f6e4d9942aa993ff6
mhyneman8 <mallory.hyneman@gmail.com>
2022-05-13 19:44:46 -0500

    add add/edit budget

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [budget, setBudget] = useState(() => {
+    const saved = localStorage.getItem("budget_amount");
+    const initialValue = JSON.parse(saved);
+    if (initialValue == null) {
+      return 0
+    } else {
+      return initialValue
+    };
+  });
+  // const [addButtonShow, setAddButtonShow] = useState(() => {
+  //   if (budget =)
+  // })
+  const [show, setShow] = useState(false);
+  const [showBudgetEdit, setShowBudgetEdit] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(budget);
+    localStorage.setItem("budget_amount", budget);
+    alert("budget changed to $" + budget);
+    setShow(!show);
+  }
+
+  const updateBudget = (e) => {
+    e.preventDefault();
+    localStorage.setItem("budget_amount", budget);
+    alert("budget changed to $" + budget);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <button
+        onClick={() => setShow(!show)}
+      >Add Budget</button>
+    
+      {show ? (
+        <form onSubmit={handleSubmit}>
+          <button
+            onClick={() => setShow(!show)}
+          >&times;</button>
+          <label>Enter Budget Amount</label>
+          <input type="text" value={budget} onChange={(event) => setBudget(event.target.value)}  />
+          <input type="submit" value="Submit" />
+        </form>
+      ) : null }
+
+      <p>
+        Budget Amount ${budget}
+      </p>
+
+      <button onClick={() => setShowBudgetEdit(!showBudgetEdit)}>
+        Edit Budget
+      </button>
+
+      {showBudgetEdit ? (
+        <form onSubmit={handleSubmit}>
+          <button
+            onClick={() => setShowBudgetEdit(!showBudgetEdit)}
+          >&times;</button>
+          <label>Enter new Budget Amount</label>
+          <input type="text" value={budget} onChange={(event) => setBudget(event.target.value)} />
+          <input type="submit" value="Submit" />
+        </form>
+      ) : null}
     </div>
   );
 }
