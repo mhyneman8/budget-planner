9bf0b07240ffdbb4e03b06f208566483de319cb8
mhyneman8 <mallory.hyneman@gmail.com>
2022-05-17 20:56:46 -0500

    add image to add category card

--- src/components/AddBudgetModal.jsx
+++ src/components/AddBudgetModal.jsx
@@ -1,37 +1,42 @@
-import { Modal, Form, Button } from 'react-bootstrap';
+import { Modal, Form, Button, Image } from 'react-bootstrap';
 import { useRef } from 'react';
 import { useBudgets } from '../contexts/BudgetsContext';
+import woman_money_phone from '../Assets/woman_money_phone.svg';
 
 export default function AddBudgetModal({ show, handleClose }) {
     const nameRef = useRef();
-    const maxRef = useRef();
+    // const maxRef = useRef();
     const { addBudget } = useBudgets();
     
     function handleSubmit(e) {
         e.preventDefault();
         addBudget({
             name: nameRef.current.value,
-            max: parseFloat(maxRef.current.value)
+            // max: parseFloat(maxRef.current.value)
         }) 
         handleClose();
     }
   
     return (
-    <Modal show={show} onHide={handleClose} >
+    <Modal show={show}>
         <Form onSubmit={handleSubmit}>
-            <Modal.Header closeButton>
-                <Modal.Title>New Budget</Modal.Title>
+            <Modal.Header>
+                <img src={woman_money_phone} alt="" className='img-fluid mx-auto' />
+                {/* <img alt="" src={require('../woman_money_phone.svg')} /> */}
+                {/* <Modal.Title>New Budget</Modal.Title> */}
             </Modal.Header>
             <Modal.Body>
                 <Form.Group className="mb-3" controlId="name">
-                    <Form.Label>Name</Form.Label>
+                    <Form.Label>Add Category Type</Form.Label>
                     <Form.Control ref={nameRef} type="text" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="max">
+                {/* <Form.Group className="mb-3" controlId="max">
                     <Form.Label>Maximum Spending</Form.Label>
                     <Form.Control ref={maxRef} type="number" required min={0} step={0.01} />
-                </Form.Group>
+                </Form.Group> */}
                 <div className='d-flex justify-content-end'>
+                    <Button variant="primary-outline" type="back" onClick={handleClose}>Back</Button>
+
                     <Button variant="primary" type="submit">Add</Button>
                 </div>
             </Modal.Body>
